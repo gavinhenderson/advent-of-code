32b33b61b8265dd59c8bde0f5026befa43047743
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2019-12-01 09:40:31 +0000

    Completed day 1

--- day1/index.js
+++ day1/index.js
@@ -1,1 +1,9 @@
-console.log("test");
+const fs = require("fs");
+const path = require("path");
+
+const file = fs.readFileSync(path.join(__dirname + "/input"), "utf-8");
+const numbers = file.split("\n").map(n => parseInt(n));
+const fuelNeeded = numbers.map(n => Math.floor(n / 3) - 2);
+const total = fuelNeeded.reduce((total, n) => total + n, 0);
+
+console.log(total);
